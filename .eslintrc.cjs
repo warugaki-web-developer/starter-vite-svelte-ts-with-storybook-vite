f040437acecc902d0da17674ec67c87212542e85
warugaki <warugaki.web.developer@gmail.com>
2022-01-01 10:00:39 +0900

    .eslintrc.cjsの設定を追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,39 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.svelte'],
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['svelte3', '@typescript-eslint'],
   rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true, // devDependenciesのimportを許可
+      },
+    ],
+    'import/no-mutable-exports': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  settings: {
+    // eslint-disable-next-line global-require
+    'svelte3/typescript': () => require('typescript'), // pass the TypeScript package to the Svelte plugin
+    // OR
+    // 'svelte3/typescript': true, // load TypeScript as peer dependency
   },
 };
